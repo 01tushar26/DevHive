b55f31b1837c92f9df74d86a59ba73fa12550ce6
Tushar Tyagi <tushartyagi2601@gmail.com>
2026-03-12 23:59:27 +0530

    feat : global reposne and exception handler

---
+++ src/main/java/com/collab/DevHive/Advice/ApiError.java
@@ -0,0 +1,15 @@
+package com.collab.DevHive.Advice;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+import java.sql.Timestamp;
+
+@Data
+@Builder
+public class ApiError {
+    private String message;
+    private HttpStatus status;
+    private Timestamp timestamp;
+}
