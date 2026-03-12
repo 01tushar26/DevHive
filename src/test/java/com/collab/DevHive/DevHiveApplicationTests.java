bef483ae0b0b387eb61045c1db38a97eeaf1ae03
Tushar Tyagi <tushartyagi2601@gmail.com>
2026-03-12 23:42:09 +0530

    setup : repo

---
+++ src/test/java/com/collab/DevHive/DevHiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collab.DevHive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevHiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
